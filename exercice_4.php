207525ea85c903812846fa6415d4de8bd1f0211e
FabienCOTE <fabien.cote@gmail.com>
2018-11-28 12:32:06 +0100

    Fin exercice

--- exercice_4.php
+++ exercice_4.php
@@ -1,20 +1,4 @@
-<?php
-if (isset($_POST['lastName']) && isset($_POST['firstName'])) {
-    if (!is_numeric($_POST['lastName']) && !is_numeric($_POST['firstName'])) {
-        $display = 'Votre nom est "' . $_POST['lastName'] . '" et votre prénom est "' . $_POST['firstName'] . '".';
-    } else {
-        $display = 'Vous devez indiquer votre nom et prénom dans le formulaire';
-    }
-} else {
-    $display = '';
-}
-?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="fr">
     <head>
         <meta charset="UTF-8" />
