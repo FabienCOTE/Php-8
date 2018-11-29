2c205e7b65862b613f2de7f4f77bba8022fe1db9
FabienCOTE <fabien.cote@gmail.com>
2018-11-29 10:45:26 +0100

    Fin

--- ex2-user.php
+++ ex2-user.php
@@ -13,12 +13,6 @@ if (isset($_GET['lastName']) && isset($_GET['firstName']) && isset($_GET['age'])
     $display = '';
 }
 ?>
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="fr">
     <head>
         <meta charset="UTF-8" />
--- exercice_3.php
+++ exercice_3.php
@@ -12,11 +12,6 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
 }
 ?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="fr">
     <head>
         <meta charset="UTF-8" />
